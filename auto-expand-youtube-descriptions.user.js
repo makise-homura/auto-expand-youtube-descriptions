940481cc591cc268529b3ed634a961c96c9188f1
makise-homura <akemi_homura@kurisa.ch>
2022-10-16 02:07:50 +0300

    Massive rework of clicker code, this also fixes auto-closing of notification panel

--- auto-expand-youtube-descriptions.user.js
+++ auto-expand-youtube-descriptions.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Auto-Expand Youtube Descriptions
-// @version  2
+// @version  3
 // @grant    none
 // @author   makise-homura
 // @include  https://youtu.be/*
@@ -9,7 +9,15 @@
 
 document.addEventListener('dom-change', function()
 {
- document.getElementById("expand").click(); // Expand descriptions
- // Array.from(document.getElementsByClassName("more-button")).forEach(b => b.click()); // Expand comments
- // Array.from(document.querySelectorAll("button.yt-spec-button-shape-next--text.yt-spec-button-shape-next--icon-leading")).forEach(b => b.click()); // Expand comment subtrees
+  // Expand descriptions
+  if (!document.getElementById('expand').hidden) {document.getElementById("expand").click();}
+
+  // Expand comment text
+  //document.querySelectorAll("span.more-button.style-scope.ytd-comment-renderer").forEach(x => (function(x) {if (x.offsetParent != null) {x.click();}})(x));
+
+  // Expand first part of comment subtrees
+  //document.querySelectorAll("#more-replies.more-button").forEach(x => (function(x) {if (!x.hidden) {x.click();}})(x));
+
+  // Expand other parts of comment subtrees
+  //document.querySelectorAll('ytd-button-renderer.style-scope.ytd-continuation-item-renderer').forEach(x => (function(x) {if (x.clientHeight > 0) {x.firstChild.firstChild.click();}})(x));
 }, false);
